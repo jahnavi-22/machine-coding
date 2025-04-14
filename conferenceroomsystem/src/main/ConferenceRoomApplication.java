dd1d60b4d1c62f9962d8a8ceb77a2ce9be014874
jahnavi-22 <jahnavi.22053@gmail.com>
2025-04-14 12:12:32 +0530

    Revert "MC-1 machine coding"
    This reverts commit d084c7c361e75d8fe502ff40ccbce17205d02c4a.

--- conferenceroomsystem/src/main/ConferenceRoomApplication.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class ConferenceRoomApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- miniwallet/src/main/models/User.java
+++
@@ -1,6 +0,0 @@
-package main.models;
-
-public class User {
-    private String name;
-
-}
